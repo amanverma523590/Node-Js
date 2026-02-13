225e278ff9fae1ba8125768007cc0c7beceb8434
Aman Verma <aman97104@gmail.com>
2026-02-13 11:56:56 +0530

    CRUD in promises

--- Promises/fp.js
+++ Promises/fp.js
@@ -1,16 +1,16 @@
 //how much file are iin the directory
 
-const fs = require('fs')
-const path = require('path');
+// const fs = require('fs')
+// const path = require('path');
 
-const fileName = "fsPromise.txt";
-const filePath = path.join(__dirname,__filename);
+// const fileName = "fsPromise.txt";
+// const filePath = path.join(__dirname,__filename);
 
-const file = __dirname;
+// const file = __dirname;
 
-fs.promises.readdir(file)
-.then((data)=> console.log(data) )
-.catch((err)=> console.log(err) )
+// fs.promises.readdir(file)
+// .then((data)=> console.log(data) )
+// .catch((err)=> console.log(err) )
 
 
 // -------------------------------------------
@@ -20,10 +20,10 @@ fs.promises.readdir(file)
 // const fs = require('fs')
 // const path = require('path');
 
-const fileName1 = __dirname;
-fs.promises.readdir(fileName1)
-.then((data)=> console.log(data))
-.catch((err)=> console.log(err))
+// const fileName1 = __dirname;
+// fs.promises.readdir(fileName1)
+// .then((data)=> console.log(data))
+// .catch((err)=> console.log(err))
  
 
 // ------------------------------------------------
@@ -37,3 +37,40 @@ fs.promises.readdir(fileName1)
 // fs.promises.writeFile(filePath,'this is data of promise','utf-8',)
 // .then( console.log('file created succesfully'))
 // .catch((err)=> console.log(err))
+
+// --------------read
+
+// const fs = require('fs')
+// const path = require('path');
+
+// const fileName = "File1.txt";
+// const filePath = path.join(__dirname,fileName);
+
+
+// fs.promises.readFile(filePath,'utf-8')
+// .then((data)=> console.log(data))
+// .catch((err)=> console.log("Error Reading file",err))
+
+//-------------------update
+
+// const fs = require('fs')
+// const path = require('path');
+
+// const fileName = "File1.txt";
+// const filePath = path.join(__dirname,fileName);
+
+// fs.promises.appendFile(filePath,'\nthis is updated data','utf-8')
+// .then(console.log('data updated successfully'))
+// .catch((err)=>console.log(err))
+
+// --------------------delete
+
+const fs = require('fs')
+const path = require('path');
+
+const fileName = "File1.txt";
+const filePath = path.join(__dirname,fileName);
+
+fs.promises.unlink(filePath)
+.then(console.log('deleted succesfully'))
+.catch((err)=>console.log(err));
