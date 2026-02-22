2c53a55ae13af10c8838c33d9d7893b4ea587559
Aman Verma <aman97104@gmail.com>
2026-02-22 21:52:08 +0530

    lsitening html

--- EXPRESS/app.js
+++ EXPRESS/app.js
@@ -1,41 +1,60 @@
-import express from 'express';
+// //PORT and ZOD
+
+// import express from 'express';
+// import {PORT} from "./env.js";
+
+// const app = express();
+
+// // const PORT = process.env.PORT || 3000 ;
+
+// app.get("/", (req,res)=>{
+//     res.send("<h1>Hellow world</h1>")
+// })
+// app.get("/about", (req,res)=>{
+//     res.send("<h1>Hellow About Page</h1>")
+// })
+// app.get("/contact", (req,res)=>{
+//    return res.send(` <div class="box">
+//     <h1 class="heading">URL Shortner</h1>
+//     <form id="form">
+//       <div>
+//         <label for="url">Enter URL :</label>
+//         <br>
+//         <input type="url" id="url" name="url" required placeholder="enter url">
+//       </div> <br>
+//       <div>
+//         <label for="shortCode">Enter Shortcode :</label> <br>
+//         <input type="text" id="shortCode" name="shortCode" required placeholder="enter shortcode">
+//       </div>
+//       <button type="submit" class="btn">Shorten</button>
+//     </form>
+
+//     <h2>Shortend URLs</h2>
+
+//     <ul id="shortend-url">
+
+//     </ul>`)
+// })
+
+// app.listen(PORT, ()=>{
+//     console.log(`Server running at port: ${PORT}`)
+// })
+
+
+//sending html and css in express
+
+import express from "express";
 import {PORT} from "./env.js";
+import path from "path"
 
 const app = express();
 
-// const PORT = process.env.PORT || 3000 ;
-
-app.get("/", (req,res)=>{
-    res.send("<h1>Hellow world</h1>")
-})
-app.get("/about", (req,res)=>{
-    res.send("<h1>Hellow About Page</h1>")
-})
-app.get("/contact", (req,res)=>{
-   return res.send(` <div class="box">
-    <h1 class="heading">URL Shortner</h1>
-    <form id="form">
-      <div>
-        <label for="url">Enter URL :</label>
-        <br>
-        <input type="url" id="url" name="url" required placeholder="enter url">
-      </div> <br>
-      <div>
-        <label for="shortCode">Enter Shortcode :</label> <br>
-        <input type="text" id="shortCode" name="shortCode" required placeholder="enter shortcode">
-      </div>
-      <button type="submit" class="btn">Shorten</button>
-    </form>
-
-    <h2>Shortend URLs</h2>
-
-    <ul id="shortend-url">
-
-    </ul>`)
-})
-
-app.listen(PORT, ()=>{
-    console.log(`Server running at port: ${PORT}`)
+app.get("/",(req,res)=>{
+  const homePagePath =  path.join(import.meta.dirname, "index.html")
+  res.sendFile(homePagePath);
 })
 
 
+app.listen(PORT,()=>{
+  console.log(`Server starting at ${PORT}`);
+});
--- EXPRESS/env.js
+++ EXPRESS/env.js
@@ -2,12 +2,17 @@
 
 
 //import zod
+// import {z} from "zod"
+
+// //cerate schema
+// const ageSchema = z.number().min(18).max(35).int();
+// const userAge = 16;
+
+
+// const parseUserAge = ageSchema.parse(userAge);
+// console.log(parseUserAge)
+
 import {z} from "zod"
 
-//cerate schema
-const ageSchema = z.number().min(18).max(35).int();
-const userAge = 16;
-
-
-const parseUserAge = ageSchema.parse(userAge);
-console.log(parseUserAge)
+const portSchema = z.coerce.number().min(2999).max(6000).default(3004);
+export const PORT = portSchema.parse(process.env.PORT);
