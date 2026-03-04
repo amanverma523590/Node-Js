6a30c8df78fe2099a0f61a66425909a57fdf64cc
Aman Verma <aman97104@gmail.com>
2026-03-04 18:00:50 +0530

    CRUD

--- MongoDb/mongo-driver.js
+++ MongoDb/mongo-driver.js
@@ -8,5 +8,37 @@ const db = client.db('mongodb_nodejs_db'); // storing the database
 
 const usersCollection = db.collection("users") // createing collection
 
-usersCollection.insertOne({"Name":"Aman","City":"Bangalore"});
-usersCollection.insertOne({"Name":"Sanam","City":"Kathmandu"}).then(console.log('done'))
+
+//⛔⛔▶️▶️Create
+// usersCollection.insertOne({"Name":"Aman","City":"Bangalore"});
+// usersCollection.insertOne({"Name":"Sanam","City":"Kathmandu"}).then(console.log('done'))
+
+// usersCollection.insertMany([ 
+//     {"Name":"Sahil","City":"Dubai"},
+//     {"Name":"Raj","City":"bangalore"} 
+// ]).then(console.log("done"))
+
+
+//▶️▶️⛔⛔Read
+// const user = usersCollection.find();
+// console.log(user)  // returns objects so use for of loop
+
+// for await (const students of user){
+//     console.log(students)
+// }
+
+//find
+
+// const users =await usersCollection.findOne({Name:"Raj"});
+// console.log(users)
+// console.log(users._id.toHexString())
+
+
+//⛔⛔👉👉Update
+
+// await usersCollection.updateOne( {Name:'Aman'},{$set:{Name:"Chaman"}} )
+
+
+//👉⛔⛔Delete
+
+await usersCollection.deleteOne({Name:'Sanam'})
